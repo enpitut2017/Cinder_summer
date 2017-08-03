8b054bb951387cec3c011ad9c88642ea7d73897b
TakumiMIYASHITA <takumi0728miyashita@gmail.com>
2017-08-03 16:54:52 +0900

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802071308) do
+ActiveRecord::Schema.define(version: 20170803074840) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
